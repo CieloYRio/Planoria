0bf42220413637ed2e88c59f3d6cf8ae1b758d79
CieloYRio <alona1986.am@gmail.com>
2025-08-24 00:17:42 +0300

    Update src/firebase.js to handle plannerState as a whole

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Current content of src/firebase.js
+
+// Example of how to handle plannerState object
+const plannerState = {
+  // Define your initial state here
+};
+
+// Function to update the planner state
+function updatePlannerState(newState) {
+  // Logic to update the planner state
+}
+
+// Function to get the planner state
+function getPlannerState() {
+  return plannerState;
+}
+
+export { updatePlannerState, getPlannerState };
